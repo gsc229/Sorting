6c4e11e3499e007a1ad24fc44420af4481906ae6
Greg <gsc229@gmail.com>
2020-02-17 15:56:49 -0500

    select sort working, but with too many swaps

---
+++ src/iterative_sorting/bubble_sort.py
@@ -0,0 +1,7 @@
+# TO-DO:  implement the Bubble Sort function below
+def bubble_sort(arr):
+
+    return arr
+
+
+bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,39 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        print(f"i loop {arr}")
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # (hint, can do in 3 loc)
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[cur_index]:
+                smallest_index = j
+                print(
+                    f"arr[j]: {arr[j]} < arr[cur_index]: {arr[cur_index]} TRUE. smallest index: {smallest_index}")
+                arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+                print(f"SWAP: {arr}")
+            else:
+                print(
+                    f"arr[j]: {arr[j]} < arr[cur_index]: {arr[cur_index]} FALSE")
 
-
-
-        # TO-DO: swap
-
-
-
+    # TO-DO: swap
 
     return arr
 
 
+selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
+
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+
+
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
